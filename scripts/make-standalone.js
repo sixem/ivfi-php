cab6c8cfa388be8c057129ae6637f312c766510b
emy <emy@five.sh>
2023-04-07 17:19:17 +0200

    Update standalone creator to work with rewritten code

--- scripts/make-standalone.js
+++ scripts/make-standalone.js
@@ -1,113 +1,139 @@
-/** Package information */
-import pck from '../package.json' assert {
-	type: 'json'
-};
-
+/** Imports */
 import fs from 'fs';
 import path from 'path';
 
+/** Data */
 const data = {
 	outFile : './build/standalone.php'
 };
 
-let stripComments = (source) =>
+/**
+ * Fired upon errors
+ * 
+ * @param {*} error 
+ */
+const onError = (error) =>
+{
+    console.error('FAILED:' + error);
+	process.exit(1);
+};
+
+/**
+ * Strips comments from a string
+ * 
+ * @param {string} source 
+ */
+const stripComments = (source) =>
 {
 	return source.replace(/(\/\*(?:(?!\*\/).|[\n\r])*\*\/)/, '');
-}
+};
 
-let minifiedBanner = () =>
-{
-	return `
-/*!
- * IVFi-PHP - ${pck.description} (${pck.version})
- *
- * [https://github.com/sixem/ivfi-php]
- *
- * Copyright (c) 2022 emy | five.sh | github.com/sixem
- * Licensed under GPL-3.0
+/**
+ * Scans the lines for a needle from a set index
+ * 
+ * @param {array}   lines 
+ * @param {string}  needle 
+ * @param {integer} fromIndex 
+ * @param {boolean} reverse 
  */
- `;
-}
-
-let replaceScriptTags = (binary) =>
+const scanFromIndex = (lines, needle, fromIndex, reverse = false) =>
 {
-	let matches = /(<script defer type="[a-z\/]+" src="\/([a-z]+\/[a-z]+\.js)(.+)"><\/script>)/g.exec(binary);
+    let index = null;
 
-	let output = new String();
+    for(let i = fromIndex;
+        reverse ? i > 0 : i < lines.length - 1;
+        reverse ? i-- : i++
+    ){
+        if(lines[i].includes(needle))
+        {
+            index = i; break;
+        }
+    }
 
-	if(matches)
-	{
-		/* create .js file path */
-		let filePath = `./build/${matches[2]}`;
+    return index;
+};
 
-		if(fs.existsSync(filePath))
-		{
-			console.log('\nReading script', '->', filePath);
-
-			/* read .js file */
-			let script = fs.readFileSync(filePath, 'binary');
-
-			/* remove script tag */
-			binary = binary.replace(matches[1], new String());
-
-			/* add full script to output */
-			output += `<script type="text/javascript">\n${minifiedBanner()}\n${stripComments(script)}</script>`;
-		} else {
-			throw new Error(`Unexisting asset file (${filePath}).`);
-		}
-	}
-
-	return [binary, output];
-}
-
-let replaceStyleTags = (binary) =>
+/**
+ * Scans lines for a needle, finds the start and end of a block, removes it and returns the search item
+ * 
+ * @param {array}   lines 
+ * @param {string}  needle 
+ * @param {string}  start 
+ * @param {string}  end 
+ * @param {regexp}  itemSearch 
+ * @param {boolean} removeComment 
+ * @param {integer} limit 
+ */
+const spliceBySearch = (lines, needle, start, end, itemSearch, removeComment = true, limit = null) =>
 {
-	let matches = /(\'<link rel="[a-z\/]+" type="text\/css" href="\/([a-z]+\/css\/[a-z]+\.css)(.+)">\')/g.exec(binary);
+    /** Indexes (lines) removed */
+    let splicedIndexes = [];
 
-	if(matches)
-	{
-		let filePath = `./build/${matches[2]}`;
+    /** Items fetched from block(s) */
+    let splicedItems = [];
 
-		if(fs.existsSync(filePath))
-		{
-			console.log('\nReading stylesheet', '->', filePath);
+    for(let i = (lines.length - 1); i > 0; i--)
+    {
+        if(lines[i].includes(needle))
+        {
+            /** Block start index */
+            let startIndex = i;
+            
+            /** Find block start */
+            const headerIndex = scanFromIndex(
+                lines, start, startIndex, true
+            );
 
-			/* read .css file */
-			let sheet = fs.readFileSync(filePath, 'utf-8');
+            if(headerIndex !== null)
+            {
+                if(removeComment && lines[headerIndex - 1].includes('/*'))
+                    startIndex = (headerIndex - 1);
 
-			let fonts = sheet.match(new RegExp(/(src\:\ ?url\(([A-Za-z0-9\.\/\-]+)\) format\("[A-Za-z0-9]+"\)\;)/g));
+                /** Find block ending */
+                const headerEnd = scanFromIndex(lines, end, headerIndex, false);
 
-			console.log('Found', fonts.length, 'font assets');
+                if(headerEnd !== null)
+                {
+                    for(let i = startIndex; i <= headerEnd; i++)
+                    {
+                        /** Check for item match */
+                        const matches = itemSearch.exec(lines[i]);
 
-			(fonts).forEach((font) =>
-			{
-				/* join asset path */
-				const fontPath = path.join('./build', (font.split('url(')[1]).split(')')[0]);
+                        if(matches)
+                        {
+                            /** Get all lines removed */
+                            const spliceSpan = [
+                                ...Array((headerEnd + 1) - startIndex).keys()
+                            ].map((i) => i + startIndex);
+                            
+                            /** Add to spliced indexes */
+                            splicedIndexes.push(...spliceSpan);
+                            splicedItems.push(matches[1]);
 
-				if(fs.existsSync(fontPath))
-				{
-					console.log('Processing ->', fontPath);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
-					/* read font as base64 */
-					let based = fs.readFileSync(fontPath, {
-						encoding : 'base64'
-					});
+        /** Check for limits */
+        if(limit && splicedItems.length >= limit)
+        {
+            break;
+        }
+    }
 
-					/* replace css font with base64 */
-					sheet = sheet.replace(font, `src: url(data:application/font-woff2;charset=utf-8;base64,${based});`);
-				} else {
-					throw new Error(`Unexisting asset file (${fontPath}).`);
-				}
-			});
+    /** Remove spliced lines */
+    for(let i = splicedIndexes.length -1; i >= 0; i--)
+    {
+        lines.splice(splicedIndexes[i],1);
+    }
 
-			binary = binary.replace(matches[1], `'<style type="text/css">${stripComments(sheet).replace(/[\']/g, `${String.fromCharCode(92)}\'`)}</style>'`);
-		} else {
-			throw new Error(`Unexisting asset file (${filePath}).`);
-		}
-	}
-
-	return binary;
-}
+    return splicedItems.length > 0 ? {
+        lines, items: splicedItems, indexes: splicedIndexes
+    } : null;
+};
 
 console.log('Building standalone ..');
 
@@ -115,46 +141,121 @@ try
 {
 	console.log('\nReading index ..');
 
-	/* read php index */
-	let source = fs.readFileSync('./build/indexer.php', 'binary');
+	/* Read PHP index */
+	const source = fs.readFileSync('./build/indexer.php', 'utf-8');
 
-	/* replace script tags, return stripped source and script data */
-	let [binary, scripts] = replaceScriptTags(source);
-
-	/* set source without script tags */
-	source = binary;
-
-	/* split source by line */
+	/* Split source by line */
 	let lines = source.split('\n');
 
-	/* iterate over lines */
-	for(let i = 0; i < lines.length; i++)
-	{
-		let line = lines[i];
+    /** Extract and remove scripts */
+    const splicedScripts = spliceBySearch(
+        lines,
+        "'type' => 'text/javascript'", '$header[]', ');',
+        /\'(\/[^\/\'\"]+\/main\.js)\?bust=%s\'/g
+    );
 
-		/* check if line is ending body */
-		if(line.includes('</body>'))
-		{
-			/* insert script data before ending body (can't use defer in standalone) */
-			lines.splice(i - 1, 0, scripts);
+    if(splicedScripts)
+    {
+        /** Update lines */
+        lines = splicedScripts.lines;
 
-			console.log('Inserting scripts at line', i - 1);
+        let scriptsData = [];
 
-			/* break loop */
-			break;
-		}
-	}
+        /** Iterate over spliced scripts */
+        for(const script of splicedScripts.items)
+        {
+            /** Construct script file relative */
+            const scriptPath = script.split('/')[1];
+            const scriptFile = `./build/${scriptPath}/${script.replace(`/${scriptPath}/`, '')}`;
 
-	/* create new source with inserted script data */
-	source = lines.join('\n');
+            console.log('\nReading script', '->', scriptFile);
 
-	/* replace stylesheets with raw css */
-	source = replaceStyleTags(source);
+            if(fs.existsSync(scriptFile))
+            {
+                scriptsData.push(fs.readFileSync(scriptFile, 'binary'));
+            } else {
+                onError(`Script file: '${scriptFile}' does not exist!`);
+            }
+        }
+
+        /** Get <body/> closing tag line */
+        const bodyEnd = scanFromIndex(lines, '</body>', lines.length - 1, true);
+
+        if(bodyEnd !== null)
+        {
+            /** Inject script data */
+            lines.splice(bodyEnd - 1, 0, scriptsData.map((data) =>
+            {
+                return `<script type="text/javascript">${data}</script>`;
+            }));
+        }
+    }
+
+    /** Extract and remove stylesheets */
+    const splicedStyles = spliceBySearch(
+        lines,
+        "$baseStylesheet", '$baseStylesheet', ');',
+        /\"(\/[^\/\'\"]+\/css\/style\.css)\?bust=%s\"/g, 1
+    );
+
+    if(splicedStyles)
+    {
+        /** Update lines */
+        lines = splicedStyles.lines;
+
+        /** Construct stylesheet relative path */
+        const stylePath = `./build/${splicedStyles.items[0]}`;
+
+        console.log('\nReading stylesheet', '->', stylePath);
+
+        if(fs.existsSync(stylePath))
+        {
+            /** Read stylesheet */
+            let stylesheetData = stripComments(fs.readFileSync(stylePath, 'utf-8'));
+
+            /** Find used fonts in stylesheet */
+            const usedFonts = stylesheetData.match(
+                new RegExp(/(src\:\ ?url\(([A-Za-z0-9\.\/\-]+)\) format\("[A-Za-z0-9]+"\)\;)/g)
+            );
+
+            console.log('Found', usedFonts.length, 'font asset(s)');
+
+            /** Iterate over used fonts */
+            for(const fontEntry of usedFonts)
+            {
+				/* Join asset path */
+				const fontPath = path.join('./build', (fontEntry.split('url(')[1]).split(')')[0]);
+
+				if(fs.existsSync(fontPath))
+                {
+                    console.log('Processing ->', fontPath);
+
+					/* Read font as Base64 and replace it with the stylesheet entry */
+                    stylesheetData = stylesheetData.replace(
+                        fontEntry, `src: url(data:application/font-woff2;charset=utf-8;base64,${fs.readFileSync(fontPath, {
+                            encoding: 'base64'
+                        })});`
+                    );
+                } else {
+                    onError(`Font file: '${fontPath}' does not exist!`);
+                }
+            }
+
+            lines.splice(
+                Math.min(...splicedStyles.indexes), 0,
+                `$baseStylesheet = '<style type="text/css">${
+                        stripComments(stylesheetData).replace(/[\']/g, `${String.fromCharCode(92)}\'`)
+                }</style>';`
+            );
+        } else {
+            onError(`Stylesheet file: '${stylePath}' does not exist!`);
+        }
+    }
 
 	console.log('\nWriting to', data.outFile);
 
 	/* write output */
-	fs.writeFileSync(data.outFile, source);
+	fs.writeFileSync(data.outFile, lines.join('\n'));
 
 	/* get output stats */
 	let stats = fs.statSync(data.outFile)
@@ -162,7 +263,5 @@ try
 	console.log(`OK .. ${stats.size} (${Math.round(((stats.size / (1024)) + Number.EPSILON) * 100) / 100} kB)`);
 } catch(error)
 {
-	console.error(error);
-
-	process.exit(1);
+	onError(error);
 }
